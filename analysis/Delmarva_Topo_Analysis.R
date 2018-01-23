9b81ff3a86d5eacb1132c27d127e57c1e0cc0cb5
Nate Jones <njones@sesync.org>
2018-01-23 10:16:17 -0500

    Readme file

--- analysis/Delmarva_Topo_Analysis.R
+++ analysis/Delmarva_Topo_Analysis.R
@@ -20,17 +20,29 @@ library(maptools) #spatial analysis
 library(plyr)     #data processing
 library(dplyr)    #data processing
 
+#Call Function DEM Processing F(x) into Memory
+source(paste0(wd,"/R/DEM_Processing.R"))
+
 #Save to backup folder
 load(paste0(dir, "/Backup/input_data.RData"))
 
-#Call Function DEM Processing F(x) into Memory
-source(paste0(wd,"/R/DEM_Processing.R"))
+#Re-define working and data directories
+wd<-"C:\\Users/njones/Desktop/workspace/Wetland_Hydrologic_Capacitance_Model"
+dir<-"X:/Validation_Modeling/WHC_BaltimoreCorner"
 
 #Run Terrain Analysis Subroutine
 DEM_Processing.fun(dem, 
                    pp.shp,
+                   wetland.shp,
                    python.path="C:\\Python27\\ArcGIS10.4\\", 
                    scratchspace="C:\\ScratchWorkspace")
+
+#Plot for shits and giggles
+plot(basin.shp)
+plot(dem.grd, add=T)
+plot(basin.shp, add=T)
+plot(pp.shp, pch=19, col="red", cex=3, add=T)
+plot(wetland.shp, pch=19, col="green", cex=3, add=T)
 
 #Backup
 save.image(paste0(dir,"/Backup/DEM_Processing.RData"))
