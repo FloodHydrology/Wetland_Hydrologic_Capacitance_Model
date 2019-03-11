55ae26fafd945df0be8038a8faf4b8ddf47b7135
Nate Jones <njones@rstudio03.research.sesync.org>
2019-03-11 19:14:39 -0400

    HUC12 Analysis Update

--- analysis/HUC12_Analysis.R
+++ analysis/HUC12_Analysis.R
@@ -29,7 +29,7 @@ library(rslurm)     # parallel computing
 # Step 2: Regional simulations------------------------------------------------------
 ####################################################################################
 #2.1 Define global simulation options------------------------------------
-n.years<-1000
+n.years<-10
 n.nodes<-16
 n.cpus<-8
 
@@ -117,6 +117,44 @@ cleanup_files(ppr)
 #f Write output to "data" folder
 write.csv(results,"data/ppr.csv")
 
+#2.3 PPR----------------------------------------------------------------------
+#a remove previous data
+remove(list=ls()[ls()!= 'n.years' &
+                   ls()!= 'n.nodes' &
+                   ls()!= 'n.cpus'  &
+                   ls()!= 'regional_analysis' &
+                   ls()!= 'wetland.hydrology'])
 
+#b run script to prep input data
+source("analysis/HUC12_Model_Input_Florida.R")
 
+#c Create wrapper function 
+florida_fun<-function(ID){
+  regional_analysis(WetID=ID,n.years, pet.VAR,precip.VAR,wetlands.shp,HUC12.shp, 
+                    catchments.shp, flowlines.shp,fac.grd, soils.shp, dem.grd, 
+                    nfw_centroid.shp, rootdepth.grd)}
 
+#d run using SLURM (this will take ~2.5 hrs)
+sopts   <- list(partition = "sesync", time = "12:00:00")
+params  <- data.frame(ID=wetlands.shp$WetID)
+florida <- slurm_apply(florida_fun, 
+                       params,
+                       add_objects = c(
+                        #Functions
+                        "wetland.hydrology", "regional_analysis",
+                        #Spatial data
+                        "fac.grd","catchments.shp","flowlines.shp","HUC12.shp",
+                        "soils.shp","wetlands.shp","dem.grd", "rootdepth.grd", 'n.years',
+                        #Climate data
+                        "precip.VAR", "pet.VAR"),
+                       nodes = n.nodes, cpus_per_node=n.cpus,
+                       pkgs=c('sp','raster','rgdal','rgeos','tidyverse'),
+                       slurm_options = sopts)
+
+#e Retrieve results
+print_job_status(florida)
+results <- get_slurm_out(florida, outtype = "table")
+cleanup_files(florida)
+
+#f Write output to "data" folder
+write.csv(results,"data/florida.csv")
