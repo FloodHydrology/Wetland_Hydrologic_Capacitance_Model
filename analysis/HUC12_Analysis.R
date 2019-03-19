a57891ba96fc5e074a67be69c827f76817aa2175
Nate Jones <njones@rstudio03.research.sesync.org>
2019-03-19 15:54:49 -0400

    remove tidyverse

--- analysis/HUC12_Analysis.R
+++ analysis/HUC12_Analysis.R
@@ -12,18 +12,24 @@
 rm(list=ls(all=TRUE))
 
 # 1b. Load packages ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Spatial Packages
 library(sp)         # for spatial analysis
 library(raster)     # for spatial analysis
 library(rgdal)      # for spatial analysis
 library(rgeos)      # for spatial analysis
 library(geosphere)  # for spatial analysis
+#Computational Packages
 library(MASS)       #distribrutional analysis
 library(markovchain)# for MCMC modeling
 library(Evapotranspiration)
+#Data munging packages
 library(lubridate)  # dealing with dates
+library(dplyr)      # for data wrangling
+library(tidyr)
+library(tibble)
+#Parralell Processing Packagaes
 library(parallel)   # parallel computing
 library(rslurm)     # parallel computing
-library(tidyverse)  # for data wrangling
 
 # 1c. Define data directory ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 backup_dir<-"/nfs/WHC-data/Regional_Analysis/backup/"
@@ -32,20 +38,17 @@ results_dir<-"/nfs/WHC-data/Figure Generation/"
 ####################################################################################
 # Step 2:Input data for regional analysis-------------------------------------------
 ####################################################################################
-#2.1 Load regional analysis function
-source("R/regional_analysis.R")
-
-#2.2 Delmarva
+#2.1 Delmarva
 source("analysis/HUC12_Model_Input_Delmarva.R")
 save.image(paste0(backup_dir,"Delmarva_Input.RData"))
 remove(list=ls()[ls()!= 'backup_dir' & ls()!= 'results_dir' & ls()!= 'regional_analysis'])
 
-#2.3 ppr
+#2.2 ppr
 source("analysis/HUC12_Model_Input_PPR.R")
 save.image(paste0(backup_dir,"PPR_Input.RData"))
 remove(list=ls()[ls()!= 'backup_dir' & ls()!= 'results_dir' & ls()!= 'regional_analysis'])
 
-#2.4 Florida
+#2.3 Florida
 source("analysis/HUC12_Model_Input_Florida.R")
 save.image(paste0(backup_dir,"Florida_Input.RData"))
 remove(list=ls()[ls()!= 'backup_dir' & ls()!= 'results_dir' & ls()!= 'regional_analysis'])
@@ -62,6 +65,7 @@ n.cpus<-8
 
 #define functions from file 
 source("R/WHC_2.R")
+source("R/regional_analysis.R")
 
 #3.2 Delmarva----------------------------------------------------------------------
 #a load input data
@@ -88,7 +92,7 @@ delmarva<- slurm_apply(dmv_fun,
                          #Climate data
                          "precip.VAR", "pet.VAR"),
                        nodes = n.nodes, cpus_per_node=n.cpus,
-                       pkgs=c('sp','raster','rgdal','rgeos','tidyverse'),
+                       pkgs=c('sp','raster','rgdal','rgeos','dplyr', 'tidyr', 'tibble'),
                        slurm_options = sopts)
 
 #3.3 PPR----------------------------------------------------------------------
@@ -128,7 +132,7 @@ ppr    <- slurm_apply(ppr_fun,
                          #Climate data
                          "precip.VAR", "pet.VAR"),
                       nodes = n.nodes, cpus_per_node=n.cpus,
-                      pkgs=c('sp','raster','rgdal','rgeos','tidyverse'),
+                      pkgs=c('sp','raster','rgdal','rgeos','dplyr', 'tidyr', 'tibble'),
                       slurm_options = sopts)
 
 #3.4 Florida----------------------------------------------------------------------
@@ -169,7 +173,7 @@ florida <- slurm_apply(florida_fun,
                         #Climate data
                         "precip.VAR", "pet.VAR"),
                        nodes = n.nodes, cpus_per_node=n.cpus,
-                       pkgs=c('sp','raster','rgdal','rgeos','tidyverse'),
+                       pkgs=c('sp','raster','rgdal','rgeos','dplyr', 'tidyr', 'tibble'),
                        slurm_options = sopts)
 
 ####################################################################################
