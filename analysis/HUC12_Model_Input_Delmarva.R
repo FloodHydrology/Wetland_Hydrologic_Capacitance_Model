c10d970ff5c84528a59773eae724ded85c8eff7e
Nate Jones <njones@rstudio03.research.sesync.org>
2019-03-26 15:25:11 -0400

    dz addition!

--- analysis/HUC12_Model_Input_Delmarva.R
+++ analysis/HUC12_Model_Input_Delmarva.R
@@ -108,6 +108,25 @@ dLe<-dLe[!duplicated(dLe[,1]),]
 wetlands.shp<-merge(wetlands.shp, dLe, by="WetID")
 remove(dLe)
 
+#3.6 Estimate dz [difference in upland and wetland elevation]
+#download function
+source("R/dz_fun.R")
+#run function
+dz<-mclapply(
+  X=wetlands.shp$WetID, 
+  FUN=dz.fun, 
+  mc.silent = T, 
+  mc.cores = detectCores()
+)
+dz<-do.call(rbind, dz)
+dz<-data.frame(dz)
+colnames(dz)<-c("WetID","dz")
+dz<-dz[dz$WetID!=0,]
+dz<-dz[!duplicated(dz[,1]),]
+
+#merge with wetland.shp
+wetlands.shp<-merge(wetlands.shp, dz, by="WetID")
+remove(dz)
 
 ####################################################################################
 # Step 4: Climate data--------------------------------------------------------------
